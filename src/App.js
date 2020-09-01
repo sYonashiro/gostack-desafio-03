151844cf46e4a80c0658d655a3c4d6da9dd0e381
Sergio Yonashiro <sergio.yonashiro@outlook.com>
2020-08-31 23:29:35 -0300

    Implementa requisitos do desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./styles.css";
+import api from './services/api';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => setRepositories(response.data));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo repositório - ${new Date()}`,
+      url: 'https://github.com/sYonashiro',
+      techs: ['ReactJS', 'Node.js']
+    });
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    
+    setRepositories(repositories.filter(item => item.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(item => (
+          <li key={item.id}>
+            {item.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(item.id)}>
+              Remover
+            </button>
+          </li>
+        ))}        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
